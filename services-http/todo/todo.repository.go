37a22accbf3952637b9e8956395287b4df1c5a6b
sing3demons <sing3demons@live.com>
2024-04-19 20:56:51 +0700

    Refactor services-http to update pagination logic in GetTodos function

--- services-http/todo/todo.repository.go
+++ services-http/todo/todo.repository.go
@@ -27,23 +27,27 @@ func (t *taskRepository) GetTodos(ctx context.Context, tq TaskQuery, log logger.
 	log.Info("TaskRepository GetTodos")
 	col := t.client.Database("todo").Collection("tasks")
 
-	var opts *options.FindOptions
+	if tq.Page == 0 {
+		tq.Page = 1
+	}
+
+	if tq.PageSize == 0 {
+		tq.PageSize = 10
+	}
+	skip := int64((tq.Page - 1) * tq.PageSize)
+	limit := int64(tq.PageSize)
+
+	opts := &options.FindOptions{
+		Skip:  &skip,
+		Limit: &limit,
+	}
+	opts.SetSort(bson.D{{Key: tq.Sort, Value: tq.Order}})
+
 	filter := bson.M{}
 
 	if tq.Status != "" {
 		filter["status"] = tq.Status
 	}
-
-	if tq.Page > 0 && tq.PageSize > 0 {
-		skip := int64((tq.Page - 1) * tq.PageSize)
-		limit := int64(tq.PageSize)
-		opts = &options.FindOptions{
-			Skip:  &skip,
-			Limit: &limit,
-		}
-	}
-
-	opts.SetSort(bson.D{{Key: tq.Sort, Value: tq.Order}})
 
 	cur, err := col.Find(ctx, filter, opts)
 	if err != nil {
