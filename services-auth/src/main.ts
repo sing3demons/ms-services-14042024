896a1bee19582057fc37c2085e7e13aa14c792b3
sing3demons <sing3demons@live.com>
2024-05-01 07:58:47 +0700

    Add core error classes and global error handler middleware

--- services-auth/src/main.ts
+++ services-auth/src/main.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 
 import router from './root-routes'
+import { globalErrorHandler } from './core'
 
 const app = express()
 
@@ -8,6 +9,8 @@ app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
+
+app.use(globalErrorHandler)
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000')
